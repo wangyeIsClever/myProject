9a6dab4eac14700df8701b4c124c2e59f78c3d03
wangye <wangye20yujing@163.com>
2019-07-15 19:01:20 +0800

    提交以前练习的文件

---
+++ shrioweb/src/main/java/com/wangye/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.wangye.controller;
+
+import com.wangye.vo.User;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class UserController {
+
+    @RequestMapping(name = "/subLogin",produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public String subLogin(User user){
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(user.getUsername(),user.getPassword());
+        try {
+            subject.login(token);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return "登录成功";
+    }
+}
