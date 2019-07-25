ce962701d46c6ebbcadda91872d0d282774847e8
wangyeIsClever <wangye20yujing@163.com>
2019-07-25 21:36:05 +0800

    Springboot多数据源配置

---
+++ spboot-multidatasource/src/main/java/com/wangye/spbootmultidatasource/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.wangye.spbootmultidatasource.controller;
+
+import com.wangye.spbootmultidatasource.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/addUser")
+    public String addUser(){
+        return userService.addUser();
+    }
+}
