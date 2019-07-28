a0039aa0729fec2406fe997fd511020634bd9e72
wangyeIsClever <wangye20yujing@163.com>
2019-07-28 23:44:02 +0800

    Springboot多数据源配置automatic数据源管理多数据源事物 未成功

--- spboot-jta-automatic/src/main/java/com/wangye/spbootjtaautomatic/config/DataSourceConfig1.java
+++ spboot-jta-automatic/src/main/java/com/wangye/spbootjtaautomatic/config/DataSourceConfig1.java
@@ -1,77 +1,60 @@
 package com.wangye.spbootjtaautomatic.config;
 
 import com.atomikos.jdbc.AtomikosDataSourceBean;
-import com.mysql.cj.jdbc.MysqlXADataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 
 @Configuration
-@MapperScan(basePackages = "com.wangye.spbootjtaautomatic.mapper1",sqlSessionTemplateRef = "test1SqlSessionFactory")
+@MapperScan(basePackages = "com.wangye.spbootjtaautomatic.mapper1",sqlSessionTemplateRef = "sqlSessionTemplate1")
 public class DataSourceConfig1 {
 
-    private DB1Properties db1Properties;
-
-    @Autowired
-    public void setDb1Properties(DB1Properties db1Properties) {
-        this.db1Properties = db1Properties;
+    @ConfigurationProperties(prefix = "spring.datasource.test1") //自定义配置数据源配置的前缀
+    @Bean(name="test1DataSource")
+    @Primary
+    public DataSource testDataSource() {
+        return new AtomikosDataSourceBean();
     }
 
-    // 配置数据源
+    @Bean(name="sqlSessionFactory1")
+    @Autowired
     @Primary
-    @Bean(name = "test1DataSource")
-    public DataSource test1DataSource() throws SQLException {
-        // 首先要注意的是MySQL使用的DataSource 是MysqlXADataSource
-        MysqlXADataSource mysqlXaDataSource = new MysqlXADataSource();
-        mysqlXaDataSource.setUrl(db1Properties.getUrl());
-        mysqlXaDataSource.setPinGlobalTxToPhysicalConnection(true);
-        mysqlXaDataSource.setPassword(db1Properties.getPassword());
-        mysqlXaDataSource.setUser(db1Properties.getUsername());
-        mysqlXaDataSource.setPinGlobalTxToPhysicalConnection(true);
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("test1DataSource") DataSource ds) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(ds);
+        factoryBean.setTypeAliasesPackage("com.wangye.spbootjtaautomatic.model1");
+        factoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper1/*.xml"));
+        return factoryBean.getObject();
+    }
 
-
-        //再者就是要把是MysqlXADataSource设置到AtomikosDataSourceBean中
-        AtomikosDataSourceBean xaDataSource = new AtomikosDataSourceBean();
-        xaDataSource.setXaDataSource(mysqlXaDataSource);
-        xaDataSource.setUniqueResourceName("test1DataSource");
-        xaDataSource.setMinPoolSize(db1Properties.getMinPoolSize());
-        xaDataSource.setMaxPoolSize(db1Properties.getMaxPoolSize());
-        xaDataSource.setMaxLifetime(db1Properties.getMaxLifetime());
-        xaDataSource.setBorrowConnectionTimeout(db1Properties.getBorrowConnectionTimeout());
-        xaDataSource.setLoginTimeout(db1Properties.getLoginTimeout());
-        xaDataSource.setMaintenanceInterval(db1Properties.getMaintenanceInterval());
-        xaDataSource.setMaxIdleTime(db1Properties.getMaxIdleTime());
-        xaDataSource.setTestQuery(db1Properties.getTestQuery());
-        return xaDataSource;
+    /**
+     * 添加事务Bean
+     * @param ds
+     * @return
+     */
+    @Primary
+    @Bean(name="transactionManager1")
+    @Autowired
+    public DataSourceTransactionManager transactionManager(@Qualifier("test1DataSource") DataSource ds) {
+        return new DataSourceTransactionManager(ds);
     }
 
     @Primary
-    @Bean(name = "test1SqlSessionFactory")
     @Autowired
-    public SqlSessionFactory test1SqlSessionFactory(@Qualifier("test1DataSource") DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-        bean.setDataSource(dataSource);
-        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(db1Properties.getMapperLocations()));
-        bean.setTypeAliasesPackage(db1Properties.getTypeAliasesPackage());
-        return bean.getObject();
+    @Bean(name="sqlSessionTemplate1")
+    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("test1DataSource") DataSource ds) throws Exception {
+        SqlSessionTemplate template = new SqlSessionTemplate(sqlSessionFactory(ds));
+        return template;
     }
-
-    @Primary
-    @Bean(name = "test1SqlSessionTemplate")
-    @Autowired
-    public SqlSessionTemplate test1SqlSessionTemplate(@Qualifier("test1SqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
-        SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory);
-        return sqlSessionTemplate;
-    }
-
 }
--- spboot-jta-automatic/src/main/java/com/wangye/spbootjtaautomatic/config/DataSourceConfig2.java
+++ spboot-jta-automatic/src/main/java/com/wangye/spbootjtaautomatic/config/DataSourceConfig2.java
@@ -1,72 +1,55 @@
 package com.wangye.spbootjtaautomatic.config;
 
 import com.atomikos.jdbc.AtomikosDataSourceBean;
-import com.mysql.cj.jdbc.MysqlXADataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 
 @Configuration
-@MapperScan(basePackages="com.wangye.spbootjtaautomatic.mapper2",sqlSessionFactoryRef = "test2SqlSessionFactory")
+@MapperScan(basePackages="com.wangye.spbootjtaautomatic.mapper2",sqlSessionFactoryRef = "sqlSessionTemplate2")
 public class DataSourceConfig2 {
 
-    private DB2Properties db2Properties;
+    @ConfigurationProperties(prefix = "spring.datasource.test2") //自定义配置数据源配置的前缀
+    @Bean(name="test2DataSource")
+    public DataSource testDataSource() {
+        return new AtomikosDataSourceBean();
+    }
+
+    @Bean(name="sqlSessionFactory2")
+    @Autowired
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("test2DataSource") DataSource ds) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(ds);
+        factoryBean.setTypeAliasesPackage("com.wangye.spbootjtaautomatic.model2");
+        factoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper2/*.xml"));
+        return factoryBean.getObject();
+    }
+
+    /**
+     * 添加事务Bean
+     * @param ds
+     * @return
+     */
+    @Bean(name="transactionManager2")
+    @Autowired
+    public DataSourceTransactionManager transactionManager(@Qualifier("test2DataSource") DataSource ds) {
+        return new DataSourceTransactionManager(ds);
+    }
 
     @Autowired
-    public void setdb2Properties(DB2Properties db2Properties) {
-        this.db2Properties = db2Properties;
+    @Bean(name="sqlSessionTemplate2")
+    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("test2DataSource") DataSource ds) throws Exception {
+        SqlSessionTemplate template = new SqlSessionTemplate(sqlSessionFactory(ds));
+        return template;
     }
-
-    // 配置数据源2
-    @Bean(name = "test2DataSource")
-    public DataSource test2DataSource() throws SQLException {
-        // 首先要注意的是MySQL使用的DataSource 是MysqlXADataSource
-        MysqlXADataSource mysqlXaDataSource = new MysqlXADataSource();
-        mysqlXaDataSource.setUrl(db2Properties.getUrl());
-        mysqlXaDataSource.setPinGlobalTxToPhysicalConnection(true);
-        mysqlXaDataSource.setPassword(db2Properties.getPassword());
-        mysqlXaDataSource.setUser(db2Properties.getUsername());
-        mysqlXaDataSource.setPinGlobalTxToPhysicalConnection(true);
-
-
-        //再者就是要把是MysqlXADataSource设置到AtomikosDataSourceBean中
-        AtomikosDataSourceBean xaDataSource = new AtomikosDataSourceBean();
-        xaDataSource.setXaDataSource(mysqlXaDataSource);
-        xaDataSource.setUniqueResourceName("test2DataSource");
-        xaDataSource.setMinPoolSize(db2Properties.getMinPoolSize());
-        xaDataSource.setMaxPoolSize(db2Properties.getMaxPoolSize());
-        xaDataSource.setMaxLifetime(db2Properties.getMaxLifetime());
-        xaDataSource.setBorrowConnectionTimeout(db2Properties.getBorrowConnectionTimeout());
-        xaDataSource.setLoginTimeout(db2Properties.getLoginTimeout());
-        xaDataSource.setMaintenanceInterval(db2Properties.getMaintenanceInterval());
-        xaDataSource.setMaxIdleTime(db2Properties.getMaxIdleTime());
-        xaDataSource.setTestQuery(db2Properties.getTestQuery());
-        return xaDataSource;
-    }
-
-    @Bean(name = "test2SqlSessionFactory")
-    @Autowired
-    public SqlSessionFactory test2SqlSessionFactory(@Qualifier("test2DataSource") DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-        bean.setDataSource(dataSource);
-        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(db2Properties.getMapperLocations()));
-        bean.setTypeAliasesPackage(db2Properties.getTypeAliasesPackage());
-        return bean.getObject();
-    }
-
-    @Bean(name = "test2SqlSessionTemplate")
-    @Autowired
-    public SqlSessionTemplate test2SqlSessionTemplate(@Qualifier("test2SqlSessionFactory") SqlSessionFactory sqlSessionFactory){
-        return new SqlSessionTemplate(sqlSessionFactory);
-    }
-
 }
