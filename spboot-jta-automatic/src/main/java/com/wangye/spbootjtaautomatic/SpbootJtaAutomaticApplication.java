04c820db787eda506f7782a1bffdfa8decaa2f3a
wangyeIsClever <wangye20yujing@163.com>
2019-07-28 20:13:16 +0800

    Springboot多数据源配置automatic数据源管理多数据源事物

---
+++ spboot-jta-automatic/src/main/java/com/wangye/spbootjtaautomatic/SpbootJtaAutomaticApplication.java
@@ -0,0 +1,13 @@
+package com.wangye.spbootjtaautomatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootJtaAutomaticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbootJtaAutomaticApplication.class, args);
+    }
+
+}
---
+++ spboot-jta-automatic/src/test/java/com/wangye/spbootjtaautomatic/SpbootJtaAutomaticApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wangye.spbootjtaautomatic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpbootJtaAutomaticApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
