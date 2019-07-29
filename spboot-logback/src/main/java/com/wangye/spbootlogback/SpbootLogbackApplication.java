4d49b5e7a350b7e1df383b4e5ed005cbc3df4246
wangyeIsClever <wangye20yujing@163.com>
2019-07-29 23:21:49 +0800

    Springboot多数据源配置logBack，版本1

---
+++ spboot-logback/src/main/java/com/wangye/spbootlogback/SpbootLogbackApplication.java
@@ -0,0 +1,13 @@
+package com.wangye.spbootlogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbootLogbackApplication.class, args);
+    }
+
+}
