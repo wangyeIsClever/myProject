7b6f3df2bcee7ec36fafdc3bc3938a5c8735d40e
wangyeIsClever <wangye20yujing@163.com>
2019-07-30 17:45:43 +0800

    Spboot+aop 配置全局web日志配置

---
+++ spboot-aop-log/src/main/java/com/wangye/spbootaoplog/SpbootAopLogApplication.java
@@ -0,0 +1,13 @@
+package com.wangye.spbootaoplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootAopLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbootAopLogApplication.class, args);
+    }
+
+}
