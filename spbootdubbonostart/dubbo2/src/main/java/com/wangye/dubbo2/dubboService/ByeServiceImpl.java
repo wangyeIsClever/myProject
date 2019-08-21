70534282216a3f5741dc6f196c850c4149697487
wangyeIsClever <wangye20yujing@163.com>
2019-08-21 09:42:23 +0800

    Springboot 整合dubbo

--- spbootdubbonostart/dubbo2/src/main/java/com/wangye/dubbo2/dubboService/ByeServiceImpl.java
+++ spbootdubbonostart/dubbo2/src/main/java/com/wangye/dubbo2/dubboService/ByeServiceImpl.java
@@ -3,7 +3,7 @@ package com.wangye.dubbo2.dubboService;
 import com.wangye.dubboapi.api.ByeService;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("byeService")
 public class ByeServiceImpl implements ByeService {
 
     @Override
