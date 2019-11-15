ac338f620ff2f6b09103568128517793470b6818
wangyeIsClever <wangye20yujing@163.com>
2019-11-15 17:03:10 +0800

    修改配置

--- spboot-mybatis-plus/src/main/java/com/wangye/spbootmybatisplus/mapper/MyUserInfoMapper.java
+++ spboot-mybatis-plus/src/main/java/com/wangye/spbootmybatisplus/mapper/MyUserInfoMapper.java
@@ -2,6 +2,8 @@ package com.wangye.spbootmybatisplus.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wangye.spbootmybatisplus.model.MyUserInfo;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MyUserInfoMapper extends BaseMapper<MyUserInfo> {
 
-    MyUserInfo selectById(@Param("id") Long id);
+    MyUserInfo selectById1(@Param("id") Long id);
+
+    IPage<MyUserInfo> getPageByUser(Page page);
 
 }
